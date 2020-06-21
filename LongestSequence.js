5aacd07ef39e6aff29c372af45e345fd4b4a064a
Prakash Choudhary <om.pcprakash@gmail.com>
2020-06-21 19:38:00 +0530

    longest sequence

---
+++ LongestSequence.js
@@ -0,0 +1,38 @@
+'use strict'
+const sequence = [2, 5, 6, 1, 8, 3, 10, 4];
+
+const set = new Set();
+
+sequence.forEach((num) => (set.add(num)));
+
+console.log(set);
+
+let i = 0;
+let length = 0;
+let maxLength = 0;
+let setIttr = set.values();
+
+while (i < sequence.length) {
+ 
+  let start = setIttr.next().value;
+  let tmep = start;
+  while (set.has(++tmep)) {
+  set.delete(tmep);
+  length++;  
+  i++;
+ }
+  
+ tmep = start;
+
+ while(set.has(--tmep)){
+  set.delete(tmep);
+  length++;  
+  i++;
+ }
+ if(length > maxLength){
+  maxLength = length;
+ }
+
+}
+
+console.log(maxLength);
